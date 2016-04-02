320a0ffc8fb63e5c302e535a0a84d37dbd65f013
Eric Templeton <erictempleton1@gmail.com>
2016-04-01 20:46:11 -0400

    adding request module and gitignore

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var Account = require('../models/account');
+var request = require('request');
 
 
 router.get('/', function(req, res) {
