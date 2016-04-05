ec67c1ba29e66de950fd4348724658fb84ccdf2e
Eric Templeton <erictempleton1@gmail.com>
2016-04-05 08:13:22 -0400

    parsing xml to json

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 var Account = require('../models/account');
 var request = require('request');
 var config = require('../config');
-//var authUtil = require('../utility/auth');
+var authUtil = require('../utility/auth');
 
 
 router.get('/', function(req, res) {
@@ -28,11 +28,10 @@ router.get('/user/:id', function(req, res) {
     });
 });
 
-/*
+
 router.post('/', function(req, res) {
     var testReq = new authUtil();
     console.log(testReq);
 });
-*/
 
 module.exports = router;
--- utility/auth.js
+++ utility/auth.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var config = require('../config');
+var parseString = require('xml2js').parseString;
 
 
 var getTabServerToken = function() {
@@ -7,14 +8,24 @@ var getTabServerToken = function() {
     var formData = '<tsRequest><credentials name=' + '"' + config.tabServer.username + '" ' +
                     'password=' + '"' + config.tabServer.password + '"' +
                      '><site contentUrl="" /></credentials></tsRequest>'
+    var headers = {'Content-Type': 'application/xml'}; 
     console.log(formData);
     console.log(tabReqUrl);
 
-    request.post(tabReqUrl, {form: {key: formData}}, function(err, resp, body) {
+    var options = {
+        url: tabReqUrl,
+        body: formData,
+        headers: headers
+    };
+
+    request.post(options, function(err, resp, body) {
         if (err) {
             console.log(err)
         } else {
-            console.log(body)
+            parseString(body, function(err, result) {
+                var parsedResp = result;
+                console.log(parsedResp);
+            });
         }
     });
 }
