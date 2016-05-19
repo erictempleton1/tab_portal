dce485c824339112e791be3d50faf5d6e26a79a1
Eric Templeton <erictempleton1@gmail.com>
2016-05-18 20:50:18 -0400

    adding notes for later

--- routes/sites.js
+++ routes/sites.js
@@ -3,6 +3,17 @@ var express = require('express'),
     passport = require('passport'),
     Account = require('../models/account');
 
+// todos -
+
+// need to re-think this route. the portal should be site first centric, so need to change
+// this route to use the site name instead.
+
+// keep the findone query, but search for the site name instead, then check if the site is private,
+// and if so, can the given user access it. redirect to homepage otherwise.
+
+// create a basic home page for each user that lists their site membership?
+
+// site pages and user pages should be seperate entities. 
 
 router.get('/:username', function (req, res) {
   Account.findOne({'username': req.params.username}, function (err, user) {
