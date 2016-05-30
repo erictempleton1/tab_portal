4af9af5d69ce5c6ce6e64de3ea354b8e38526a9b
Eric Templeton <erictempleton1@gmail.com>
2016-05-30 19:28:27 -0400

    adding user page

--- routes/user/user.js
+++ routes/user/user.js
@@ -2,10 +2,26 @@ var express = require('express'),
     router = express.Router(),
     Account = require('../../models/account'),
     Sites = require('../../models/sites');
-    
+
+// /user/eric  
 router.get('/:username', function (req, res) {
-    // page to display sites a user is joined to
-    res.send('placeholder for user page!');
+    if (req.user) {
+        if (req.user.isAdmin || req.user.username === req.params.username) {
+            Sites.find({allowedUsers: req.params.username}).exec()
+            .then(function (sites) {
+                res.render('user/user_page', {sites: sites});
+            }).catch(function (err) {
+                req.flash('info', 'There was an error loading sites >> ' + err);
+                res.redirect('/user/' + req.params.username);
+            });
+        } else {
+            req.flash('info', 'Unauthorized');
+            res.redirect('/');
+        }
+    } else {
+        req.flash('info', 'Unauthorized');
+        res.redirect('/');
+    }
 });
 
 module.exports = router;
