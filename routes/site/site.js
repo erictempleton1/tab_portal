bb81a3eb3cd332cacf00769352bf125cee2e0d3a
Eric Templeton <erictempleton1@gmail.com>
2016-05-30 20:58:33 -0400

    adding sites page and query

--- routes/site/site.js
+++ routes/site/site.js
@@ -4,7 +4,28 @@ var express = require('express'),
     Sites = require('../../models/sites');
     
 router.get('/:sitename', function (req, res) {
-    res.send('Site!');
+    if (req.user) {
+        Sites.findOne({siteName: req.params.sitename}).exec()
+        .then(function (site) {
+            if (site) {
+                if (site.allowedUsers.indexOf(req.user.username) >= 0 || req.user.isAdmin) {
+                    res.render('site/site_page', {site: site});
+                } else {
+                    req.flash('info', 'User is not authorized to view this site');
+                    res.redirect('/');
+                }
+            } else {
+                req.flash('info', 'Site not found');
+                res.redirect('/');
+            }
+        }).catch(function (err) {
+            req.flash('info', 'There was an error loading site >> ' + err);
+            res.redirect('/');
+        });
+    } else {
+        req.flash('info', 'Unauthorized');
+        res.redirect('/');
+    }
 });
 
 module.exports = router;
