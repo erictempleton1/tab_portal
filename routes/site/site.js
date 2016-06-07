c7b1b92c5bed180f942b3214c5932b60039e10af
Eric Templeton <erictempleton1@gmail.com>
2016-06-06 20:46:14 -0400

    adding post request for viz site render

--- routes/site/site.js
+++ routes/site/site.js
@@ -30,4 +30,31 @@ router.get('/:sitename', function (req, res) {
     }
 });
 
+// todo - finish post request here, add forms to ejs, and write utility function 
+// for trusted ticket!
+router.post('/:sitename', function(req, res) {
+    if (req.user) {
+        Sites.findOne({siteName: req.params.sitename}).exec()
+        .then(function (site) {
+            if (site) {
+                if (site.allowedUsers.indexOf(req.user.username) >= 0 || req.user.isAdmin) {
+                    // todo - post request logic here!
+                } else {
+                    req.flash('info', 'User is not authorized to view this site');
+                    res.redirect('/');
+                }
+            } else {
+                req.flash('info', 'Site not found');
+                res.redirect('/');
+            }
+        }).catch(function (err) {
+            req.flash('info', 'There was an error loading site >> ' + err);
+            res.redirect('/');
+        });
+    } else {
+        req.flash('info', 'Unauthorized');
+        res.redirect('/');
+    }
+});
+
 module.exports = router;
