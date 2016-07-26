8a8363e0e46be8d6013fdd836ca1d4b6b9a2946b
Eric Templeton <erictempleton1@gmail.com>
2016-07-25 21:26:58 -0400

    moving post request for trusted ticket inside get req for page

--- routes/site/site.js
+++ routes/site/site.js
@@ -13,38 +13,18 @@ router.get('/:sitename', function (req, res) {
             if (site) {
                 // make sure that the user is allowed to view the site or an admin
                 if (site.allowedUsers.indexOf(req.user.username) >= 0 || req.user.isAdmin) {
-                    res.render('site/site_page', {site: site, user: req.user});
-                } else {
-                    req.flash('info', 'User is not authorized to view this site');
-                    res.redirect('/');
-                }
-            } else {
-                req.flash('info', 'Site not found');
-                res.redirect('/');
-            }
-        }).catch(function (err) {
-            req.flash('info', 'There was an error loading site >> ' + err);
-            res.redirect('/');
-        });
-    } else {
-        req.flash('info', 'Unauthorized');
-        res.redirect('/');
-    }
-});
-
-// todo - finish post request here, add forms to ejs, and write utility function 
-// for trusted ticket!
-router.post('/:sitename', function(req, res) {
-    if (req.user) {
-        Sites.findOne({siteName: req.params.sitename}).exec()
-        .then(function (site) {
-            if (site) {
-                if (site.allowedUsers.indexOf(req.user.username) >= 0 || req.user.isAdmin) {
-                    tabServerUtil.getTrustedTicket(req.user.username, req.params.sitename)
-                    .then(function (ticket) {
+                    tabServerUtil.getTrustedTicket(req.user.username, req.params.sitename).
+                    then(function (ticket) {
                         console.log(ticket);
+                        res.render('site/site_page', {
+                            site: site,
+                            user: req.user,
+                            trustTicket: ticket
+                        });
+                    }).catch(function (err) {
+                        req.flash('info', 'Error loading site');
+                        res.redirect('/');
                     });
-                    res.render('site/site_page', {site: site, user: req.user});
                 } else {
                     req.flash('info', 'User is not authorized to view this site');
                     res.redirect('/');
