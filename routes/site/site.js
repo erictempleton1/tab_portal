87796840ec445ad9b69bd002f8f06787a2d4dad3
Eric Templeton <erictempleton1@gmail.com>
2016-07-26 21:16:38 -0400

    adding function to build trust ticket url

--- routes/site/site.js
+++ routes/site/site.js
@@ -4,6 +4,18 @@ var express = require('express'),
     tabServerUtil = require('../../utility/tabServerAuth'),
     Account = require('../../models/account'),
     Sites = require('../../models/sites');
+
+function ticketUrl (siteUrl, trustedTicket) {
+    // todo - add more url checks? index of views?
+    var splitUrl = siteUrl.split("/t/"),
+        finalUrl;
+    if (splitUrl.length > 1) {
+        finalUrl = splitUrl[0] + "/trusted/" + trustedTicket + "/t/" + splitUrl[1];
+        return finalUrl;
+    } else {
+        finalUrl = false;
+    }
+}
     
 router.get('/:sitename', function (req, res) {
     // render the site page that displays the given workbook
@@ -15,12 +27,18 @@ router.get('/:sitename', function (req, res) {
                 if (site.allowedUsers.indexOf(req.user.username) >= 0 || req.user.isAdmin) {
                     tabServerUtil.getTrustedTicket(req.user.username, req.params.sitename).
                     then(function (ticket) {
-                        console.log(ticket);
-                        res.render('site/site_page', {
-                            site: site,
-                            user: req.user,
-                            trustTicket: ticket
-                        });
+                        // build the url and pass to template
+                        var renderUrl = ticketUrl(site.siteUrl, ticket);
+                        if (renderUrl) {
+                            res.render('site/site_page', {
+                                site: site,
+                                user: req.user,
+                                renderUrl: renderUrl
+                            });
+                        } else {
+                            req.flash('info', 'Error building render url');
+                            res.redirect('/');
+                        }
                     }).catch(function (err) {
                         req.flash('info', 'Error loading site');
                         res.redirect('/');
