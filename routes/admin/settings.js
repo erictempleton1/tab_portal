1672f8468d8f972865d6099e74d36918a35fc518
erictempleton1 <erictempleton1@gmail.com>
2016-11-20 13:35:42 -0500

    finishing post request and re-formatting date

--- routes/admin/settings.js
+++ routes/admin/settings.js
@@ -71,4 +71,39 @@ router.get('/config/edit', function (req, res) {
     }
 });
 
+/**
+ * Post request to update config values.
+ * Only 1 config document is expected and accepted here.
+ */
+router.post('/config/edit', function (req, res) {
+    if (req.user && req.user.isAdmin) {
+        TabServerConfig.find({}).exec()
+        .then(function(config){
+            if (config.length == 1) {
+                var serverObj = config[0];
+                serverObj.tabServerUsername = req.body.tabServerUsername;
+                serverObj.tabServerPassword = req.body.tabServerPassword;
+                serverObj.tabServerUrl = req.body.tabServerUrl;
+                serverObj.updatedDate = Date.now();
+                serverObj.save(function(saveErr) {
+                    if (!saveErr) {
+                        req.flash('info', 'Config updated');
+                        res.redirect('/admin/settings');
+                    } else {
+                        req.flash('info', 'Error updating config settings');
+                        res.redirect('/admin/settings');
+                    }
+                })
+            } else {
+                req.flash('info', 'Expecting one config doc. Found ' + config.length);
+                res.redirect('/admin/settings');
+            }
+        }).catch(function(err) {
+            console.log(err);
+            req.flash('info', 'Unable to save config settings due to internal error');
+            res.redirect('/admin/settings');
+        })
+    }
+});
+
 module.exports = router;
