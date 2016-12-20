62ab8fef5694f244cff9aec3104caae73612a30c
erictempleton1 <erictempleton1@gmail.com>
2016-12-20 06:23:28 -0500

    removing config page

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,91 +1,12 @@
 var express = require('express'),
-    router = express.Router(),
-    authUtil = require('../../utility/tabServerAuth'),
-    ServerToken = require('../../models/serverToken'),
-    TabServerConfig = require('../../models/tabServerConfig');
+    router = express.Router();
 
-
+/**
+ * Render admin index
+ */
 router.get('/', function (req, res) {
-    // main admin page
     if (req.user && req.user.isAdmin) {
         res.render('admin/admin', {user: req.user});
-    } else {
-        req.flash('info', 'Unauthorized');
-        res.redirect(403, '/');
-    }
-});
-
-router.post('/', function (req, res) {
-    if (req.user && req.user.isAdmin) {
-        TabServerConfig.find({}).exec()
-        .then(function (config) {
-            if (config.length == 0) {
-                req.checkBody('submit', 'Missing submit value').notEmpty();
-                req.getValidationResults()
-                .then(function(submitValResult) {
-                    if (submitValResult.isEmpty()) {
-                        var submitVal = req.body.submit;
-                        if (submitVal === 'Add Config') {
-                            req.checkBody('tabServerUsername', 'Missing username').notEmpty();
-                            req.checkBody('tabServerPassword', 'Missing password').notEmpty();
-                            req.checkBody('tabServerUrl', 'Missing Url').notEmpty();
-                            req.getValidationResults()
-                            .then(function(valResult) {
-                                if (valResult.isEmpty()) {
-                                    var configInfo = new TabServerConfig({
-                                        tabServerUsername: req.body.tabServerUsername,
-                                        tabServerPassword: req.body.tabServerPassword,
-                                        tabServerUrl: req.body.tabServerUrl,
-                                        addedDate: Date.now(),
-                                        updatedDate: Date.now()
-                                    });
-                                    configInfo.save(function (saveErr) {
-                                        if (saveErr) {
-                                            req.flash('info', 'Error on save: ' + saveErr);
-                                            res.redirect('/');
-                                        } else {
-                                            req.flash('info', 'Config settings added');
-                                            res.redirect('/admin');
-                                        }
-                                    });
-                                } else {
-                                    res.send('Validation error');
-                                }
-                            });
-                        } else if (submitVal === 'Skip For Now') {
-                            // todo - save empty string fields here
-                            var emptyConfigInfo = new TabServerConfig({
-                                tabServerUsername: null,
-                                tabServerPassword: null,
-                                tabServerUrl: null,
-                                addedDate: Date.now(),
-                                updatedDate: Date.now()
-                            });
-                            emptyConfigInfo.save(function (emptySaveErr) {
-                                if (emptySaveErr) {
-                                    req.flash('info', 'Error on save: ' + emptySaveErr);
-                                    res.redirect('/');
-                                } else {
-                                    req.flash('info', 'Skipped setting config');
-                                    res.redirect('/admin');
-                                }
-                            });
-                        } else {
-                            req.flash('info', 'Unknown submit data');
-                            res.redirect('/');
-                        }
-                    } else {
-                        res.send('Validation error');
-                    }
-                });
-            } else {
-                req.flash('info', 'Invalid request');
-                res.redirect(302, '/admin');
-            }
-        }).catch(function (err) {
-            req.flash('info', 'An error occurred: ' + err);
-            res.redirect('/admin');
-        });
     } else {
         req.flash('info', 'Unauthorized');
         res.redirect(403, '/');
--- routes/admin/settings.js
+++ routes/admin/settings.js
@@ -6,7 +6,6 @@ var express = require('express'),
 
 /**
  * Render settings page for admins to view config and other settings.
- * Only 1 config document is expected and accepted here.
  */
 router.get('/', function (req, res) {
     if (req.user && req.user.isAdmin) {
@@ -35,74 +34,6 @@ router.get('/', function (req, res) {
     } else {
         req.flash('info', 'Unauthorized');
         res.redirect('/');
-    }
-});
-
-/**
- * Render edit config settings page for admins.
- * Only 1 config document is expected and accepted here.
- */
-router.get('/config/edit', function (req, res) {
-    if (req.user && req.user.isAdmin) {
-        TabServerConfig.find({}).exec()
-        .then(function(config) {
-            if (config.length == 1) {
-                res.render(
-                    'admin/edit_config',
-                    {
-                        user: req.user,
-                        config: config,
-                        moment: moment
-                    }
-                );
-            } else {
-                req.flash(
-                    'info',
-                    'Error loading config. Found ' + config.length + ' records'
-                );
-            }
-        }).catch(function (findErr) {
-            req.flash('info', 'An error occurred: ' + findErr);
-            red.redirect('/admin');
-        });
-    } else {
-        req.flash('info', 'Unauthorized');
-        res.redirect('/');
-    }
-});
-
-/**
- * Post request to update config values.
- * Only 1 config document is expected and accepted here.
- */
-router.post('/config/edit', function (req, res) {
-    if (req.user && req.user.isAdmin) {
-        TabServerConfig.find({}).exec()
-        .then(function(config){
-            if (config.length == 1) {
-                var serverObj = config[0];
-                serverObj.tabServerUsername = req.body.tabServerUsername;
-                serverObj.tabServerPassword = req.body.tabServerPassword;
-                serverObj.tabServerUrl = req.body.tabServerUrl;
-                serverObj.updatedDate = Date.now();
-                serverObj.save(function(saveErr) {
-                    if (!saveErr) {
-                        req.flash('info', 'Config updated');
-                        res.redirect('/admin/settings');
-                    } else {
-                        req.flash('info', 'Error updating config settings');
-                        res.redirect('/admin/settings');
-                    }
-                })
-            } else {
-                req.flash('info', 'Expecting one config doc. Found ' + config.length);
-                res.redirect('/admin/settings');
-            }
-        }).catch(function(err) {
-            console.log(err);
-            req.flash('info', 'Unable to save config settings due to internal error');
-            res.redirect('/admin/settings');
-        })
     }
 });
 
