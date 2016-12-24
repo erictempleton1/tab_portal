5dbb79ae8833d2601d1bffd9e6a388b795458c1d
erictempleton1 <erictempleton1@gmail.com>
2016-12-24 14:12:42 -0500

    adding ensureAdmin to settings page render

--- routes/admin/settings.js
+++ routes/admin/settings.js
@@ -1,38 +1,34 @@
 var express = require('express'),
     router = express.Router(),
-    moment = require('moment');
+    moment = require('moment'),
+    util = require('../../utility/utility');
 
 /**
  * Render settings page for admins to view config and other settings.
  */
-router.get('/', function (req, res) {
-    if (req.user && req.user.isAdmin) {
-        TabServerConfig.find({}).exec()
-        .then(function(config) {
-            if (config.length == 1) {
-                res.render(
-                    'admin/settings',
-                    {
-                        user: req.user,
-                        config: config,
-                        moment: moment
-                    }
-                );
-            } else {
-                req.flash(
-                    'info',
-                    'Error loading config. Found ' + config.length + ' records'
-                );
-                res.redirect('/admin');
-            }
-        }).catch(function (findErr) {
-            req.flash('info', 'An error occurred: ' + findErr);
+router.get('/', util.ensureAdmin, function (req, res) {
+    TabServerConfig.find({}).exec()
+    .then(function(config) {
+        if (config.length == 1) {
+            res.render(
+                'admin/settings',
+                {
+                    user: req.user,
+                    config: config,
+                    moment: moment
+                }
+            );
+        } else {
+            req.flash(
+                'info',
+                'Error loading config. Found ' + config.length + ' records'
+            );
             res.redirect('/admin');
-        });
-    } else {
-        req.flash('info', 'Unauthorized');
-        res.redirect('/');
-    }
+        }
+    }).catch(function (findErr) {
+        req.flash('info', 'An error occurred: ' + findErr);
+        res.redirect('/admin');
+    });
 });
 
 module.exports = router;
