98f69f57b7f5df2f09695a66e39f67d5ca4ffa0e
erictempleton1 <erictempleton1@gmail.com>
2016-12-25 14:26:34 -0500

    adding new site validator

--- validators/admin.js
+++ validators/admin.js
@@ -1,3 +1,23 @@
+exports.validateNewSitePost = function(req, res, next) {
+    req.checkBody('siteName', 'Invalid site name').notEmpty();
+    req.checkBody('vizUrl', 'Invalid site url').notEmpty();
+    req.checkBody('allowedUsers', 'Invalid allowed users list').notEmpty();
+    req.checkBody('isTabServerViz', 'Invalid tab server viz setting').notEmpty().isBoolean();
+    req.checkBody('trustedLogin', 'Invalid trusted login setting').notEmpty().isBoolean();
+    req.getValidationResult()
+    .then(function(valResult) { 
+        if (valResult.isEmpty()) {
+            return next();
+        }
+        res.status(400).json(
+            {
+                message: 'validation error',
+                error: valResult.array()
+            }
+        );
+    });
+}
+
 exports.validateSiteEditPost = function(req, res, next) {
     req.checkBody('siteName', 'Invalid site name').notEmpty();
     req.checkBody('vizUrl', 'Invalid site url').notEmpty();
