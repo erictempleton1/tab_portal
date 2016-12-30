7ecfc96a6018ba64c5b9b98da3a017a839e2d5a8
erictempleton1 <erictempleton1@gmail.com>
2016-12-29 21:36:00 -0500

    removing settings page for now

--- routes/admin/settings.js
+++ routes/admin/settings.js
@@ -7,28 +7,7 @@ var express = require('express'),
  * Render settings page for admins to view config and other settings.
  */
 router.get('/', util.ensureAdmin, function (req, res) {
-    TabServerConfig.find({}).exec()
-    .then(function(config) {
-        if (config.length == 1) {
-            res.render(
-                'admin/settings',
-                {
-                    user: req.user,
-                    config: config,
-                    moment: moment
-                }
-            );
-        } else {
-            req.flash(
-                'info',
-                'Error loading config. Found ' + config.length + ' records'
-            );
-            res.redirect('/admin');
-        }
-    }).catch(function (findErr) {
-        req.flash('info', 'An error occurred: ' + findErr);
-        res.redirect('/admin');
-    });
+    res.send('In progress...');
 });
 
 module.exports = router;
