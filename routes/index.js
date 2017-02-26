0ea337119ad4615ffdd5b7bdd257d2de68b2d2b6
erictempleton1 <erictempleton1@gmail.com>
2017-02-26 07:23:20 -0500

    removing create admin options from ui

--- routes/index.js
+++ routes/index.js
@@ -11,52 +11,7 @@ var express = require('express'),
 
 
 router.get('/', function (req, res) {
-	Account.find({}).exec()
-	.then(function (users) {
-		if (users.length > 0) {
-			res.render('index', {user: req.user});
-		} else if (users.length == 0) {
-			// no users have been found so prompt user to create an admin
-			res.render('create_admin', {user: req.user});
-		}
-	}).catch(function (findErr) {
-		util.log(3, 'index get request error: ' + findErr);
-		req.flash('info', 'Error loading index');
-		res.redirect('/');
-	});
-});
-
-router.post('/', valIndex.validateCreateAdminUser, function (req, res) {
-	// post request for first startup when there is no admin user
-	Account.find({}).exec()
-	.then(function (users) {
-		// only allow this method when there are no other users
-		if (users.length > 0) {
-			req.flash('info', 'Invalid request');
-			res.redirect(301, '/');
-		} else {
-			var regInfo = {
-				username: util.cleanString(req.body.username),
-				isAdmin: true,
-				regDate: Date.now(),
-				lastLogin: Date.now()
-			};
-			Account.register(new Account(regInfo), req.body.password, function (regErr, account) {
-				if (regErr) {
-					util.log(3, 'index post request error creating admin user: ' + regErr);
-					req.flash('info', 'Error creating admin user');
-					res.redirect('/');
-				} else {
-					req.flash('info', 'Admin user created. Please log in.');
-					res.redirect('/login');
-				}
-			});
-		}
-	}).catch(function (findErr) {
-		util.log(3, 'index post request error: ' + findErr);
-		req.flash('info', 'Error loading index');
-		res.redirect('/');
-	});
+	res.render('index', {user: req.user});
 });
 
 router.get('/login', function (req, res) {
