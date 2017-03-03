b7603eed222490569186690491ac73a2b394547d
erictempleton1 <erictempleton1@gmail.com>
2017-03-02 21:00:43 -0500

    starting admin tests

--- test/admin/test_admin.js
+++ test/admin/test_admin.js
@@ -7,17 +7,44 @@ var app = require('../../app'),
     MongoClient = require('mongodb').MongoClient,
     dbSettings = require('../../db_config'),
     Account = require('../../models/account');
+    // agent = chai.request.agent(app);
 
 chai.use(chaiHttp);
 
-describe('GET admin unauthorized', function() {
-    it('should redirect to home', function(done) {
-        chai.request(app)
-        .get('/admin')
-        .end(function(err, res) {
-            assert.equal(res.redirects.length, 1);
-            assert(res.redirects[0].endsWith('/'));
-            done();
+describe('admin tests', function() {
+    before(function(done) {
+        MongoClient.connect(dbSettings.dbUri.testing)
+        .then(function(dbCOnn) {
+            dbConn.dropDatabase(function(err, result) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        })
+        .then(function() {
+            var regInfo = {
+                username: 'admin',
+                isAdmin: true,
+                regDate: Date.now(),
+                lastLogin: Date.now()
+            };
+            Account.register(new Account(regInfo), 'admin', function(err, user) {
+                if (err) {
+                    console.log(err);
+                }
+            });
         });
     });
+
+    after(function(done) {
+        MongoClient.connect(dbSettings.dbUri.testing)
+        .then(function(dbConn) {
+            dbConn.dropDatabase(function(err, result) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        });
+        done();
+    })
 });
