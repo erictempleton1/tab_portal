94f55eb8c8f33a191b2039824575831da2693ee1
erictempleton1 <erictempleton1@gmail.com>
2017-04-10 07:24:59 -0400

    adding moment formatted date

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,8 +1,11 @@
 var express = require('express'),
     router = express.Router(),
     util = require('../../utility/utility'),
-    AdminActivity = require('../../models/adminActivity');
+    AdminActivity = require('../../models/adminActivity'),
+    moment = require('moment');
 
+
+// todo - add moment formatted date to ejs
 
 /**
  * Render admin index
@@ -11,12 +14,26 @@ router.get('/', util.ensureAdmin, function (req, res) {
     AdminActivity.find({}).exec()
     .then(function(activities) {
         console.log(activities);
-        res.render('admin/admin', {user: req.user, activities: activities});
+        res.render(
+            'admin/admin', 
+            {
+                user: req.user, 
+                activities: activities,
+                moment: moment
+            }
+        );
     })
     .catch(function(err) {
         console.log(err);
         var errMsg = 'Error loading activities';
-        res.render('admin/admin', {user: req.user, activities: [{error: errMsg}]})
+        res.render(
+            'admin/admin', 
+            {
+                user: req.user, 
+                activities: [{error: errMsg}],
+                moment: moment
+            }
+        );
     });
 });
 
